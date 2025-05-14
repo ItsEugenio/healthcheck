579cc6a46f3dc90f0fbe2ca8571f9cbf3c185ec8
ItsEugenio <223730@ids.upchiapas.edu.mx>
2025-05-13 22:56:22 -0600

    changes on server.js file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
-  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Eugenio Gordillo Argüello' });
+  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Eugenio Gordillo Argüello!' });
 });
 
 app.get('/age', (_req, res) => {
