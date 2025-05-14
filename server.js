9b503a58f907c0540c2dbc4914c4296ab0560528
ItsEugenio <223730@ids.upchiapas.edu.mx>
2025-05-14 00:50:42 -0600

    changes on server.js file

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.get('/age', (_req, res) => {
   res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), age: '21 years old :]' });
 });
 
-app.get('/nationality'), (_req, res) =>{
+app.get('/nationality', (_req, res) =>{
   res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), nationality: 'Mexico, Chiapas' });
-}
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
